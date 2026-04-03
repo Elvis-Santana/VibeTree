02d2dd901e4b78ed6e8cdc182800e862d461b1ac
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-04-02 23:26:10 -0300

    refactor: renomado Perfil

---
+++ VibeTree.Domain/Entity/Perfil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibeTree.Domain.Entity;
+
+public class Perfil : EntityBase
+{
+    public string Cor { get;  }
+    public string Descricao { get; }
+    public string ImagemUrl { get; }
+
+    public Guid IdUser { get; }
+
+    public Perfil(Guid id, DateTime createdAt, DateTime updatedAt,string cor,string descricao,string imagemUrl,Guid idUser) : base(id, createdAt, updatedAt)
+    {
+        Cor = cor;
+        Descricao = descricao;
+        ImagemUrl = imagemUrl;
+        IdUser = idUser;
+    }
+}
