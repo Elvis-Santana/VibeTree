ca7bee014fb6b8cf23829965da63fbb97a78f2fd
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-04-02 23:10:41 -0300

    feat : add entity de LinkClick

---
+++ VibeTree.Domain/Entity/LinkClick.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibeTree.Domain.Entity;
+
+public class LinkClick : EntityBase
+{
+    public Guid IdLink { get; }
+    public long ClickCount { get; }
+
+    public LinkClick(Guid id, DateTime createdAt, DateTime updatedAt, Guid idLink, long clickCount) : base(id, createdAt, updatedAt)
+    {
+        IdLink = idLink;
+        ClickCount = clickCount;
+    }
+}
