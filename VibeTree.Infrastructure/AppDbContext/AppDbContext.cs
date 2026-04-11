e89c90972ac0e99e2435ddd8494e19ecdf0b4803
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-04-11 18:06:32 -0300

    feat : configurações das entitys de Link e User

--- VibeTree.Infrastructure/AppDbContext/AppDbContext.cs
+++ VibeTree.Infrastructure/AppDbContext/AppDbContext.cs
@@ -4,14 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VibeTree.Domain.Entity;
 
 namespace VibeTree.Infrastructure.AppDbContext;
 
 public class AppDbContext : DbContext
 {
-
-    public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) :base(dbContextOptions)
+    public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
     {
 
+    }
+
+    public DbSet<User> Users { get;  set; }
+    public  DbSet<Link> Links { get;  set; }
+
+
+   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Link>()
+           .HasKey(l => l.Id);
+
+
+        modelBuilder.Entity<User>(u =>
+        {
+            u.HasKey(x => x.Id);
+            u.Property(x => x.Email).IsUnicode();
+             u.HasMany(x => x.Links)
+            .WithOne()
+            .HasForeignKey(l => l.IdUser)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        });
+            
+        base.OnModelCreating(modelBuilder);
     }
 }
