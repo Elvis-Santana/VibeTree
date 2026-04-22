2c38d9c65b8104143e13298f5879f8730efe10f7
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-04-22 10:08:28 -0300

    feat : add configuração de JWT

---
+++ VibeTree.Application/ConfigureApplication/ConfigureApplication.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibeTree.Application.ConfigureApplication;
+
+public static class ConfigureApplication
+{
+
+    public static void ConfigureServicesApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        var jwt = configuration.GetSection("JwtSettings");
+        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["secret_key"]));
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwt["Issuer"],
+                    //ValidAudience = jwt[Audience],
+                    IssuerSigningKey = secretKey
+                };
+            });
+
+
+
+    }
+}
