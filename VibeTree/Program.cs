9f5366d81b0fba0e5a301d8c795e775ffd1a5440
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-04-22 10:09:50 -0300

    feat : add configuração de JWT para OpenApi e add ddScoped para TokenService

--- VibeTree/Program.cs
+++ VibeTree/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System.Reflection.Metadata;
+using VibeTree.Application.Auth;
+using VibeTree.Application.ConfigureApplication;
 using VibeTree.Application.Interfaces;
 using VibeTree.Application.User;
 using VibeTree.Application.User.Get;
@@ -14,14 +19,60 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(optionsAction =>
 {
-    string connection = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new ArgumentException();
+    string connection = builder.Configuration
+    .GetConnectionString("DefaultConnection") ?? throw new ArgumentException();
+
     optionsAction.UseMySql(connection, ServerVersion.AutoDetect(connection));
 
 });
 
+builder.Services.AddOpenApi(optionsAction =>
+{
+    optionsAction.AddDocumentTransformer((document, contex, CancellationToken) =>
+    {
+        var requirements = new Dictionary<string, OpenApiSecurityScheme>
+        {
+            ["Bearer"] = new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = JwtBearerDefaults.AuthenticationScheme,
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Insira o token JWT"
+            }
+        };
+
+
+        document.Components ??= new OpenApiComponents();
+        document.Components.SecuritySchemes = requirements;
+
+        document.SecurityRequirements.Add(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference =new  OpenApiReference{
+                        Id="Bearer",
+                        Type = ReferenceType.SecurityScheme
+
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+
+        return Task.CompletedTask;
+    });
+  
+});
+
+builder.Services.ConfigureServicesApplication(builder.Configuration);
+
 builder.Services.AddTransient<IHandler<CreateUserCommand, UserResponse>, CreateUserHandler>();
 
 builder.Services.AddTransient<IHandler<GetAllUserQuery, List<UserResponse>>, GetAllUserHandler>();
+
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 
 var app = builder.Build();
@@ -41,15 +92,15 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+var forecast = Enumerable.Range(1, 5).Select(index =>
+    new WeatherForecast
+    (
+        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        Random.Shared.Next(-20, 55),
+        summaries[Random.Shared.Next(summaries.Length)]
+    ))
+    .ToArray();
+return forecast;
 })
 .WithName("GetWeatherForecast");
 
