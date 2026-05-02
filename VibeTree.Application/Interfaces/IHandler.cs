55b9dac4ae8d586a3eeac244c048d7c0a5387df3
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-02 10:05:47 -0300

    feat : add interfaces de Ihandler e IResquest

---
+++ VibeTree.Application/Interfaces/IHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Application.User.Get;
+
+namespace VibeTree.Application.Interfaces;
+
+public interface IHandler<TResquest, TResponse>  where TResquest : IResquest<TResponse>
+{
+     Task<TResponse> HandleAsync(TResquest command);
+}
+public interface IHandler <TResponse>
+{
+
+
+    Task<TResponse> HandleAsync( );
+
+
+}
