64debe6d92925a19a09ca6fc295935f2cae8b525
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-04 18:02:00 -0300

    feat : add validação de CreateUserCommand

---
+++ VibeTree.Application/User/Create/CreateUserValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Application.Result;
+using VibeTree.Application.User.Login;
+
+namespace VibeTree.User.CreateUser;
+
+public class CreateUserValidator : AbstractValidator<CreateUserCommand>
+{
+
+    public CreateUserValidator()
+    {
+        RuleFor(x => x.name)
+            .NotEmpty().WithMessage(Error.NameEmpty.Message)
+            .MinimumLength(3).WithMessage(Error.NameMinimumLength.Message);
+
+        RuleFor(x => x.email)
+           .NotEmpty().WithMessage(Error.EmailEmpty.Message)
+           .EmailAddress().WithMessage(Error.EmailAddress.Message);
+
+        RuleFor(x => x.password)
+            .NotEmpty().WithMessage(Error.PasswordEmpty.Message)
+            .MinimumLength(6).WithMessage(Error.PasswordMinimumLength.Message);
+    }
+
+}
