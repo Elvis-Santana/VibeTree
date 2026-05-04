18dff5a27684816fd3bc5f9bf864513cecdff3ef
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-04 18:29:05 -0300

    feat : add Result

--- VibeTree.Application/User/Create/CreateUserCommand.cs
+++ VibeTree.Application/User/Create/CreateUserCommand.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VibeTree.Application.Interfaces;
+using VibeTree.Application.Result;
 using VibeTree.Application.User;
 
 namespace VibeTree.User.CreateUser;
 
-public record class CreateUserCommand(string name,string password,string email) :IResquest<Userlogin>;
+public record class CreateUserCommand(string Name,string Password,string Email) :IResquest<Result<Userlogin>>;
 
 
 
