48b6bfef8e3768bc75cd5a82239df2fc8e82e192
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-03 21:54:54 -0300

    feat : add LoginValidator para  validar compos de LoginQuery

---
+++ VibeTree.Application/User/Login/LoginValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Domain.Entity;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace VibeTree.Application.User.Login;
+
+public class LoginValidator :AbstractValidator<LoginQuery>
+{
+    public LoginValidator()
+    {
+        RuleFor(x => x.email)
+            .NotEmpty().WithMessage("email não pode ser vazio")
+            .EmailAddress().WithMessage("Formato de email inválido.");
+
+        RuleFor(x => x.password).NotEmpty().WithMessage("password não pode ser vazio")
+            .MinimumLength(6).WithMessage("password deve conter no mínimo 6 caracteres.");
+    }
+
+  
+}
