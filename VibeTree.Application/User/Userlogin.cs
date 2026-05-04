8a720678783fd7c6fa699fe4f7227dd09d2f7d5f
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-03 21:57:19 -0300

    feat : add Userlogin retorno para login

---
+++ VibeTree.Application/User/Userlogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Application.Auth;
+
+namespace VibeTree.Application.User;
+
+public record Userlogin(Guid Id,string Name,string Email,string Token);
+
+
