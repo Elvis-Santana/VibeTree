09bcb4a5221d599273c87aa06fef74f10ca868bd
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-04 17:28:58 -0300

    feat : add constructor em Result com parametro Error

--- VibeTree.Application/Result/Result.cs
+++ VibeTree.Application/Result/Result.cs
@@ -10,14 +10,16 @@ namespace VibeTree.Application.Result;
 
 public record Result(bool isSuccess, List<Error>? error = default)
 {
-    public List<Error>? Error  => error;
+    public List<Error>? Error { get; set; } = error;
 
-    public bool IsSuccess   => isSuccess;
-    public static Result Success()=> new Result(true, null);
-    
-    public static Result Failure(List<Error> error) => new Result(false,error);
-    public static Result Failure(Error error) => new(false, [error]);
+    public bool IsSuccess => isSuccess;
+    public static Result Success() => new Result(true, null);
+
+    public static Result Failure(List<Error> error) => new Result(false, error);
+    public static Result Failure(Error error) => new Result(false, new List<Error>() { error });
+
     public static implicit operator Result(List<Error> error) => Failure(error);
+
     public static implicit operator Result(Error error) => Failure(error);
 
 
@@ -27,14 +29,17 @@ public record Result<T>: Result
 {
     public T Value { get; } = default!;
 
-    private Result(T value ) : base(true,null) => this.Value = value;
+    private Result(T value) : base(true, null) => this.Value = value;
+
     public Result(List<Error> error) : base(false, error) { }
+
+    public Result(Error error) : base(false, new List<Error> { error }) { }
 
     public static implicit operator Result<T>(T value) => new Result<T>(value);
 
-    public static implicit operator Result<T>(List<Error> error) => new(error);
+    public static implicit operator Result<T>(List<Error> error) => new Result<T>(error);
 
-    public static implicit operator Result<T>(Error error) => new(error);
+    public static implicit operator Result<T>(Error error) => new Result<T>(error);
 
 
 
