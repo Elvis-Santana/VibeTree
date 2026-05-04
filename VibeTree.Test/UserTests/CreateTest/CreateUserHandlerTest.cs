5a2625259ea898caf09aeb82bfa6c93618a637d8
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-04 18:43:14 -0300

    test :add teste de verificação alertas de erro

--- VibeTree.Test/UserTests/CreateTest/CreateUserHandlerTest.cs
+++ VibeTree.Test/UserTests/CreateTest/CreateUserHandlerTest.cs
@@ -1,16 +1,18 @@
 ﻿using Bogus;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VibeTree.Application.Auth;
 using VibeTree.Application.Interfaces;
+using VibeTree.Application.Result;
 using VibeTree.Application.User;
 using VibeTree.Infrastructure.AppDbContext;
 using VibeTree.User.CreateUser;
-
 namespace VibeTree.Test.UserTests.CreateUserTest;
 
 public class CreateUserHandlerTest
@@ -24,26 +26,58 @@ public class CreateUserHandlerTest
               .UseInMemoryDatabase(new Guid().ToString())
               .Options;
 
-           _context = new (option);
+        _context = new(option);
     }
 
     [Fact]
     public async Task CreateUserHandler_Should_Create_User()
     {
+        var config = JwtBuildConfig.BuildConfig();
+        var service = new TokenService(config);
 
         var faker = new Faker<CreateUserCommand>("pt_BR")
-        .CustomInstantiator(f => new ( f.Person.FullName, f.Person.Email,  f.Internet.Password()));
+        .CustomInstantiator(f =>
+        new(f.Person.FullName, BCrypt.Net.BCrypt.HashPassword(f.Internet.Password()), f.Person.Email));
 
         CreateUserCommand user = faker.Generate();
-        IHandler<CreateUserCommand, UserResponse> commandHandler = new CreateUserHandler(_context);
+        IHandler<CreateUserCommand, Result<Userlogin>> commandHandler = new CreateUserHandler(_context, service, new CreateUserValidator());
 
 
-        UserResponse result = await commandHandler.HandleAsync(user);
+        Result<Userlogin> result = await commandHandler.HandleAsync(user);
 
-        result.Name.Should().Be(user.name);
-        result.Email.Should().Be(user.email);
-        result.Id.Should().NotBeEmpty();
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Name.Should().Be(user.Name);
+        result.Value.Email.Should().Be(user.Email);
+        result.Value.Id.Should().NotBeEmpty();
+        result.Value.Token.Should().NotBeNullOrWhiteSpace();
 
+
+
+    }
+
+    [Fact]
+    public async Task CreateUserHandler_Should_Return_Error_When_Invalid_Data()
+    {
+        var config = JwtBuildConfig.BuildConfig();
+        var service = new TokenService(config);
+
+        CreateUserCommand user = new(string.Empty, string.Empty, string.Empty);
+
+        IHandler<CreateUserCommand, Result<Userlogin>> commandHandler = new CreateUserHandler(_context, service, new CreateUserValidator());
+
+        Result<Userlogin> result = await commandHandler.HandleAsync(user);
+        result.Should().NotBeNull();
+
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Contains(Error.EmailAddress).Should().BeTrue();
+        result.Error.Contains(Error.EmailEmpty).Should().BeTrue();
+
+        result.Error.Contains(Error.NameMinimumLength).Should().BeTrue();
+        result.Error.Contains(Error.NameEmpty).Should().BeTrue();
+
+        result.Error.Contains(Error.PasswordEmpty).Should().BeTrue();
+        result.Error.Contains(Error.PasswordMinimumLength).Should().BeTrue();
 
 
     }
