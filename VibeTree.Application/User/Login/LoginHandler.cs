1b324cef4657851a9797acfb7e3d1fadb0c3ea50
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-04 15:27:58 -0300

    feet : add validação LoginQuery

--- VibeTree.Application/User/Login/LoginHandler.cs
+++ VibeTree.Application/User/Login/LoginHandler.cs
@@ -7,32 +7,39 @@ using System.Text;
 using System.Threading.Tasks;
 using VibeTree.Application.Auth;
 using VibeTree.Application.Interfaces;
+using VibeTree.Application.Result;
 using VibeTree.Infrastructure.AppDbContext;
-
+using BCryptNet = BCrypt.Net.BCrypt;
 namespace VibeTree.Application.User.Login;
 
 public class LoginHandler(
     AppDbContext appDbContext,
     ITokenService tokenService,
     IValidator<LoginQuery> validator
-    ) : IHandler<LoginQuery, Userlogin>
+    ) : IHandler<LoginQuery, Result<Userlogin>>
 {
     private readonly AppDbContext _appDbContext = appDbContext;
-    private readonly IValidator<LoginQuery> _validator = validator;
+    private readonly IValidator<LoginQuery> _valiator = validator;
     private readonly ITokenService _tokenService = tokenService;
 
-    public async Task<Userlogin> HandleAsync(LoginQuery query)
+    public async Task<Result<Userlogin>> HandleAsync(LoginQuery query)
     {
 
-        Domain.Entity.User? user = await this._appDbContext.Users
+        var validationResult = _valiator.Validate(query);
+
+        if (!validationResult.IsValid)
+            return validationResult.Errors.Select(e => new Error(e.ErrorMessage)).ToList();
+
+        Domain.Entity.User? user = await this._appDbContext
+            .Users
             .FirstOrDefaultAsync(u => u.Email.Equals(query.email));
 
-        var validationResult = _validator.Validate(query);
 
-        if (!validationResult.IsValid )
-            return null;
+        if (user is null || !(BCryptNet.Verify(query.password, user?.PasswordHash)))
+            return Error.InvalidCredentials;
 
         Token token = await this._tokenService.CriarToken(user!);
+
         return new Userlogin(user!.Id, user.Name, user.Email, token.token);
 
     }
