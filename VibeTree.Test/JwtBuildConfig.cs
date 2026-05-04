e917a30e5f822f16b53383cee5e2c71e74a839ed
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-04 15:42:26 -0300

    feat : add JwtBuildConfig para configura ConfigurationBuilder

---
+++ VibeTree.Test/JwtBuildConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace VibeTree.Test;
+
+public static class JwtBuildConfig
+{
+
+    public static IConfiguration BuildConfig(
+        string secret = "chave_de_teste_muito_longa_e_secreta_12345",
+        string issuer = "http://localhost"
+    )
+    {
+        var inMemorySettings = new Dictionary<string, string>
+        {
+            {"JwtSettings:secret_key", secret},
+             {"JwtSettings:Issuer", issuer}
+        };
+
+        return new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings )
+            .Build();
+    }
+}
