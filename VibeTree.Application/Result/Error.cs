83c9eb4eb5715d10670763017b884ba48d012e4b
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-04 15:23:32 -0300

    feat :  add Error para criar alertas  de validação

---
+++ VibeTree.Application/Result/Error.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibeTree.Application.Result;
+
+public  record struct Error(string Message)
+{
+    public  static Error NameEmpty = 
+        new ("o nome não pode ser vazio");
+
+    public static Error EmailEmpty = 
+        new ("email não pode ser vazio");
+
+    public static Error EmailAddress = 
+        new ("Formato de email inválido.");
+
+    public static Error PasswordEmpty = 
+        new("senha não pode ser vazio");
+
+    public static Error PasswordMinimumLength =
+        new("senha deve conter no mínimo 6 caracteres");
+
+
+    public static Error InvalidCredentials =
+        new("Credenciais inválidas");
+
+
+}
