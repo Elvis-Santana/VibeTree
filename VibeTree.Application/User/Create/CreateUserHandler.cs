1665bc956959999c8c7a0a5e5084c089b6a9ab98
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-04 18:30:25 -0300

    feat : add validador de CreateUserCommand e aleta de errors

--- VibeTree.Application/User/Create/CreateUserHandler.cs
+++ VibeTree.Application/User/Create/CreateUserHandler.cs
@@ -1,25 +1,38 @@
-﻿using VibeTree.Application.Auth;
+﻿using FluentValidation;
+using VibeTree.Application.Auth;
 using VibeTree.Application.Interfaces;
+using VibeTree.Application.Result;
 using VibeTree.Application.User;
 using VibeTree.Infrastructure.AppDbContext;
 
 namespace VibeTree.User.CreateUser;
 
-public class CreateUserHandler (AppDbContext appDbContext, ITokenService tokenService) :IHandler<CreateUserCommand, Userlogin>
+public class CreateUserHandler (
+    AppDbContext appDbContext,
+    ITokenService tokenService,
+    IValidator<CreateUserCommand> validator
+    ) :IHandler<CreateUserCommand, Result<Userlogin>>
 {
     private readonly AppDbContext _appDbContext = appDbContext;
     private readonly ITokenService _tokenService = tokenService;
+    private readonly IValidator<CreateUserCommand> _validator = validator;
 
 
-    public async Task<Userlogin> HandleAsync(CreateUserCommand command)
+    public async Task<Result<Userlogin>> HandleAsync(CreateUserCommand command)
     {
+        var validationResult = await _validator.ValidateAsync(command);
+
+        if (!validationResult.IsValid)
+            return validationResult.Errors.Select(e => new Error(e.ErrorMessage)).ToList();
+        
+
         Domain.Entity.User user = new (
             Guid.NewGuid(),
             DateTime.UtcNow,
             DateTime.UtcNow,
-            command.name,
-            BCrypt.Net.BCrypt.HashPassword(command.password),
-            command.email
+            command.Name,
+            BCrypt.Net.BCrypt.HashPassword(command.Password),
+            command.Email
         );
 
         await _appDbContext.Users.AddAsync(user);
