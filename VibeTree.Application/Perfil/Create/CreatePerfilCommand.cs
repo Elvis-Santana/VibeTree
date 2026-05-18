a5ed57c4b7a5fa1a5b81f7c316c1dc2f2ecce7d9
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-18 16:53:18 -0300

    feat : add handler de create perfil

---
+++ VibeTree.Application/Perfil/Create/CreatePerfilCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Application.Interfaces;
+using VibeTree.Application.Result;
+using VibeTree.Application.User;
+
+namespace VibeTree.Application.Perfil.Create;
+
+public record CreatePerfilCommand(string Cor, string Descricao, string ImagemUrl, string Slug, Guid IdUser): IResquest<Result<PerfilResponse>>;
+
+
---
+++ VibeTree.Application/Perfil/Create/CreatePerfilHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Application.Interfaces;
+using VibeTree.Application.Result;
+using VibeTree.Infrastructure.AppDbContext;
+
+namespace VibeTree.Application.Perfil.Create;
+
+public class CreatePerfilHandler(AppDbContext appDbContext) : IHandler<CreatePerfilCommand, Result<PerfilResponse>>
+{
+    private readonly AppDbContext _appDbContext = appDbContext;
+    public async Task<Result<PerfilResponse>> HandleAsync(CreatePerfilCommand command)
+    {
+
+        Domain.Entity.Perfil perfil = new Domain.Entity.Perfil(
+               Guid.NewGuid(),
+               DateTime.Now,
+               DateTime.Now,
+               command.Cor,
+               command.Descricao,
+               command.ImagemUrl,
+               command.Slug,
+               command.IdUser
+        );
+
+        await _appDbContext.perfils.AddAsync(perfil);
+        await _appDbContext.SaveChangesAsync();
+       
+
+        return new PerfilResponse(perfil.Id, perfil.Descricao, perfil.ImagemUrl, perfil.Slug, perfil.IdUser);
+
+
+    }
+}
