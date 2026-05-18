9780c2dc939c8642219af92b6cdc91c60ad04a7a
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-18 16:53:47 -0300

    feat : add PerfilResponse para retorno de create perfil

---
+++ VibeTree.Application/Perfil/PerfilResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibeTree.Application.Perfil;
+
+public record PerfilResponse(Guid Id, string Descricao, string ImagemUrl, string Slug, Guid IdUser);
+
+
