8bc0bebbb2cd49b615655811b71d83d6ee56cbc6
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-22 08:53:43 -0300

    refactor : Atualizar interface de handler e resquest

--- VibeTree.Application/Interfaces/IHandler.cs
+++ VibeTree.Application/Interfaces/IHandler.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VibeTree.Application.Result;
 using VibeTree.Application.User.Get;
 
 namespace VibeTree.Application.Interfaces;
 
-public interface IHandler<TResquest, TResponse>  where TResquest : IResquest<TResponse>
+public interface IHandler<in TResquest, TResponse>  where TResquest : IResquest<TResponse>
 {
-     Task<TResponse> HandleAsync(TResquest command);
+     Task<Result<TResponse>> HandleAsync(TResquest command);
 }
 public interface IHandler <TResponse>
 {
-
-
-    Task<TResponse> HandleAsync( );
+    Task<Result<TResponse>> HandleAsync( );
 
 
 }
