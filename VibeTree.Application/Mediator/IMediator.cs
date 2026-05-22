a2de54eb76670e318ce62e0f63bd07586a73c7b4
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-22 08:52:24 -0300

    feat : add mediator de handlers

---
+++ VibeTree.Application/Mediator/IMediator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Application.Interfaces;
+using VibeTree.Application.Result;
+
+namespace VibeTree.Application.Mediator;
+
+public interface IMediator
+{
+    public Task<Result<IResponse>> SendAync<IResponse>(IResquest<IResponse> resquest);
+}
---
+++ VibeTree.Application/Mediator/Mediator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using VibeTree.Application.Interfaces;
+using VibeTree.Application.Mediator;
+
+namespace VibeTree.Application.Mediator;
+
+public sealed class Mediator (IServiceProvider provider) : IMediator
+{
+    public async Task<Result.Result<IResponse>> SendAync<IResponse>(IResquest<IResponse> request)
+    {
+
+        
+        Type type = request.GetType();
+
+        var handlerType = typeof(IHandler<,>).MakeGenericType(type, typeof(IResponse));
+
+        dynamic handler = provider.GetRequiredService(handlerType);
+
+        return await handler.HandleAsync((dynamic)request);
+        
+    }
+}
