d24fbd4b8fcb3d7060578bb18814b178b39beb91
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-22 09:51:45 -0300

    feat : add FactoryMed para retornar IMediator

---
+++ VibeTree.Test/FactoryMed.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Application.Interfaces;
+using VibeTree.Application.Mediator;
+using VibeTree.Application.Perfil;
+using VibeTree.Application.Perfil.Create;
+using VibeTree.Infrastructure.AppDbContext;
+
+namespace VibeTree.Test;
+
+public class FactoryMed 
+{
+    public static IMediator CreateMediatorWithHandler<TRequest, TResponse>(IHandler<TRequest, TResponse> handler) where TRequest :IRequest<TResponse>
+    {
+        IServiceProvider provider = Substitute.For<IServiceProvider>();
+
+        provider.GetService(Arg.Any<Type>()).Returns(handler);
+
+        return new Mediator(provider);
+
+    }
+}
