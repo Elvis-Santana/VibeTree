49f166556ba204878d7b114f52ac0234e5ac6645
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-05-22 17:43:02 -0300

    feat : add handler progurar Perfil por Id

---
+++ VibeTree.Application/Perfil/Get/GetById/GetPerfilByIdHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Application.Interfaces;
+using VibeTree.Application.Result;
+using VibeTree.Infrastructure.AppDbContext;
+
+namespace VibeTree.Application.Perfil.Get.GetById;
+
+public class GetPerfilByIdHandler(
+    AppDbContext appDbContext
+    ) : IHandler<GetPerfilByIdQuery, PerfilResponse>
+{
+    public async Task<Result<PerfilResponse>> HandleAsync(GetPerfilByIdQuery command)
+    {
+       var result = await appDbContext
+            .perfils
+            .FirstOrDefaultAsync(p => p.Id.Equals(Guid.Parse(command.id)));
+
+        return result is null
+          ? new Error("perfil não encontrado")
+          : new PerfilResponse(
+                result!.Id, 
+                result.Descricao,
+                result.ImagemUrl,
+                result.Slug,
+                result.IdUser
+          );
+
+    }
+}
---
+++ VibeTree.Application/Perfil/Get/GetById/GetPerfilByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Application.Interfaces;
+
+namespace VibeTree.Application.Perfil.Get.GetById;
+
+public record GetPerfilByIdQuery(string id) : IRequest<PerfilResponse>;
+
+
