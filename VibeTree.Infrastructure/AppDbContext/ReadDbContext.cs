26aefc1a12ec546cfc2e3a3eb1f22a12b94406d7
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-06-05 22:43:20 -0300

    feat : add contexts

---
+++ VibeTree.Infrastructure/AppDbContext/ReadDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Application.Interfaces;
+using VibeTree.Domain.Entity;
+using UserEntity = VibeTree.Domain.Entity.User;
+
+namespace VibeTree.Infrastructure.AppDbContext;
+
+public class ReadDbContext(DbContextOptions<ReadDbContext> options) : AbstractDbContext(options), IReadDbContext;
+
+   
+
+   
+
---
+++ VibeTree.Infrastructure/AppDbContext/WriteDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using VibeTree.Application.Interfaces;
+using VibeTree.Domain.Entity;
+
+namespace VibeTree.Infrastructure.AppDbContext;
+
+public class WriteDbContext(DbContextOptions<WriteDbContext> dbContextOptions) : AbstractDbContext(dbContextOptions), IWriteDbContext;
+
+  
+
+    
+
