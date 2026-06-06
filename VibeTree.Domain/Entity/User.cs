8b2755d7dafdb04a9126f155dde782f0f6ef14a5
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-06-05 22:41:49 -0300

    feat : add setName,setEmail e PasswordHash

--- VibeTree.Domain/Entity/User.cs
+++ VibeTree.Domain/Entity/User.cs
@@ -8,9 +8,9 @@ namespace VibeTree.Domain.Entity;
 
 public class User : EntityBase
 {
-    public string Name { get; } 
-    public string PasswordHash { get; }
-    public string Email { get; }
+    public string Name { get; private set ; } 
+    public string PasswordHash { get; private set; }
+    public string Email { get; private set; }
     public virtual ICollection<Link> Links { get; }
 
     private User() : base() { }
@@ -23,5 +23,19 @@ public class User : EntityBase
         Name = name;
         PasswordHash = passwordHash;
         Email = email;
+    }
+
+    public void SetName(string ?Name)
+    {
+        this.Name = Name ?? this.Name;
+    }
+
+    public void SetEmail(string ?Email) { 
+     this.Email = Email??this.Email;
+    }
+
+    public void setPasswordHash(string? PasswordHash )
+    {
+        this.PasswordHash = PasswordHash??this.PasswordHash;
     }
 }
