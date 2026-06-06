caf742776034820f375106bf4ba5063fce3480aa
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-06-05 22:44:03 -0300

    feat : add Mediator concreto

---
+++ VibeTree.Infrastructure/Mediator/Mediator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VibeTree.Application.Interfaces;
+using VibeTree.Application.Result;
+
+namespace VibeTree.Infrastructure.Mediator;
+
+public sealed class Mediator(IServiceProvider provider) : IMediator
+{
+    public async Task<Result<IResponse>> SendAsync<IResponse>(IRequest<IResponse> request) 
+    {
+
+        Type type = request.GetType();
+
+        Type handlerType = typeof(IHandler<,>).MakeGenericType(type, typeof(IResponse));
+
+        dynamic handler = provider.GetRequiredService(handlerType);
+
+        return await handler.HandleAsync((dynamic)request);
+        
+    }
+
+  
+}
