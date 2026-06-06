41ec72ec0a39b7b6410f50eade8ce8e570b2ea64
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-06-05 22:45:10 -0300

    feat : add  Querys de sincronização de bando de dados

---
+++ VibeTree.Infrastructure/Query/QueryJobSynchronizePerfilDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using VibeTree.Application.Interfaces.IQueryJob;
+using VibeTree.Application.QuerySync;
+using VibeTree.Domain.Entity;
+
+namespace VibeTree.Infrastructure.Query;
+
+public class QueryJobSynchronizePerfilDb : IQueryJobSynchronize<SyncData<Perfil>>
+{
+
+    private readonly Channel<SyncData<Perfil>> _channel  = Channel.CreateBounded<SyncData<Perfil>>(new BoundedChannelOptions(1_000)
+    {
+        SingleWriter = true,
+        FullMode = BoundedChannelFullMode.Wait
+    });
+
+    public bool HasPending => _channel.Reader.TryPeek(out _);
+
+    public ValueTask AddJob(SyncData<Perfil> jobSynchronize, CancellationToken cancellationToken = default)
+        => _channel.Writer.WriteAsync(jobSynchronize, cancellationToken);
+
+
+
+    public  IAsyncEnumerable<SyncData<Perfil>> ReadAllAsync(CancellationToken cancellationToken)
+        =>  _channel.Reader.ReadAllAsync(cancellationToken);
+    
+}
---
+++ VibeTree.Infrastructure/Query/QueryJobSynchronizeUserDb.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Channels;
+using VibeTree.Application.Interfaces.IQueryJob;
+using VibeTree.Application.QuerySync;
+
+namespace VibeTree.Infrastructure.Query;
+
+public class QueryJobSynchronizeUserDb : IQueryJobSynchronize<SyncData<Domain.Entity.User>>
+{
+
+    private readonly Channel<SyncData<Domain.Entity.User>> _channel = Channel.CreateBounded<SyncData<Domain.Entity.User>>(
+        new BoundedChannelOptions(1_000){
+            FullMode = BoundedChannelFullMode.Wait,
+            SingleReader = true
+        }
+    );
+
+    public bool HasPending => _channel.Reader.TryPeek(out _);
+
+    public async ValueTask AddJob(SyncData<Domain.Entity.User> jobSynchronize, CancellationToken cancellationToken)
+       => await _channel.Writer.WriteAsync(jobSynchronize, cancellationToken);
+    
+
+    public  IAsyncEnumerable<SyncData<Domain.Entity.User>> ReadAllAsync(CancellationToken cancellationToken)
+        =>  _channel.Reader.ReadAllAsync(cancellationToken);
+    
+}
