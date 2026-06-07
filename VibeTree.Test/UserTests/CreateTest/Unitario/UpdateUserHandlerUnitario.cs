9fa7f43f844d058e6a39480efeda4db91bdc5e83
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-06-06 21:20:03 -0300

    test : adds testes de ErrorNotFaund ,DeveAtualizar_SemSenha e DeveAtualizar_ComSenh

--- VibeTree.Test/UserTests/CreateTest/Unitario/UpdateUserHandlerUnitario.cs
+++ VibeTree.Test/UserTests/CreateTest/Unitario/UpdateUserHandlerUnitario.cs
@@ -1,18 +1,12 @@
-﻿using Bogus;
-using FluentAssertions;
+﻿using FluentAssertions;
 using FluentValidation;
 using FluentValidation.Results;
 using NSubstitute;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VibeTree.Application.Auth;
 using VibeTree.Application.Interfaces;
 using VibeTree.Application.Interfaces.IQueryJob;
 using VibeTree.Application.QuerySync;
+using VibeTree.Application.Result;
 using VibeTree.Application.User.Update;
 using ValidationResult = FluentValidation.Results.ValidationResult;
 
@@ -26,8 +20,7 @@ public class UpdateUserHandlerUnitario
     private readonly IQueryJobSynchronize<SyncData<Domain.Entity.User>> _jobMock = Substitute.For<IQueryJobSynchronize<SyncData<Domain.Entity.User>>>();
     private readonly IValidator<UpdateUserCommand> _validatorMock = Substitute.For<IValidator<UpdateUserCommand>>();
 
-    public UpdateUserHandler UpdateUser() => 
-        new UpdateUserHandler(_dbMock, _tokenMock, _validatorMock, _jobMock);
+    public UpdateUserHandler UpdateUser() => new (_dbMock, _tokenMock, _validatorMock, _jobMock);
 
     [Fact]
     public async Task HandleAsync_DeveRetornarErroUpdateUserCommand_QuandoValidacaoFalhar()
@@ -45,42 +38,127 @@ public class UpdateUserHandlerUnitario
             .ValidateAsync(Arg.Any<UpdateUserCommand>())
             .Returns(erros);
 
-        var createUserCommand = new UpdateUserCommand("", "na","@@","123");
+        var createUserCommand = new UpdateUserCommand(string.Empty, "na","@@","123");
         var result = await UpdateUser().HandleAsync(createUserCommand);
 
-        await _dbMock.DidNotReceive()
-            .SaveChangesAsync();
+        erros.Errors.Select(x => x.ErrorMessage)
+            .SequenceEqual(result.Errors!.Select(e => e.Message))
+            .Should()
+            .BeTrue();
+
+       await _dbMock.DidNotReceive()
+          .SaveChangesAsync();
         await _jobMock.DidNotReceiveWithAnyArgs()
-            .AddJob(default!);
+            .AddJobAsync(default!);
         await _tokenMock.DidNotReceiveWithAnyArgs()
             .CriarToken(default!);
-
-        for (int i = 0; i < erros.Errors.Count(); i++)
-            erros.Errors[i].ErrorMessage.Should().Be(result.Errors[i].Message);
-        
-
 
     }
 
     [Fact]
-    public async Task HandleAsync_DeveRetornarErro_notFaund()
+    public async Task HandleAsync_DeveRetornarErro_NotFaund()
     {
         _validatorMock
              .ValidateAsync(Arg.Any<UpdateUserCommand>())
              .Returns(new ValidationResult());
 
-        var createUserCommand = new UpdateUserCommand(Guid.NewGuid().ToString(), "", "", "");
-        _dbMock.Users.FindAsync(Arg.Any<Guid>()).Returns(ValueTask.FromResult<Domain.Entity.User>(null));
+        var createUserCommand = new UpdateUserCommand(Guid.NewGuid().ToString(), string.Empty, string.Empty, string.Empty);
+        _dbMock.Users.FindAsync(Arg.Any<Guid>())!.Returns(ValueTask.FromResult<Domain.Entity.User>(null));
 
         var result = await UpdateUser().HandleAsync(createUserCommand);
+        result.IsSuccess.Should().BeFalse();
+        result.Errors!.Should().Contain(Error.NotFound);
+
+       
 
         await _dbMock.DidNotReceive().SaveChangesAsync();
-        await _jobMock.DidNotReceiveWithAnyArgs().AddJob(default!);
+        await _jobMock.DidNotReceiveWithAnyArgs().AddJobAsync(default!);
         await _tokenMock.DidNotReceiveWithAnyArgs().CriarToken(default!);
 
-        result.IsSuccess.Should().BeFalse();
-        result.Errors[0].Message.Should().Contain("not faund");
     }
+
+    [Fact]
+    public async Task HandleAsync_DeveAtualizar_SemSenha()
+    {
+        var userId = Guid.NewGuid();
+        var initialHash = BCrypt.Net.BCrypt.HashPassword("initial-hash");
+        var user = new Domain.Entity
+            .User(userId, DateTime.UtcNow, DateTime.UtcNow, "oldName", initialHash, "old@email");
+
+        var command = new UpdateUserCommand(userId.ToString(), "newName", "new@email", string.Empty);
+
+        _validatorMock
+              .ValidateAsync(Arg.Any<UpdateUserCommand>())
+              .Returns(new ValidationResult());
+
+        _dbMock.Users.FindAsync(Arg.Any<Guid>())!
+            .Returns(ValueTask.FromResult(user));
+
+        _dbMock.SaveChangesAsync().Returns(Task.FromResult(1));
+        _tokenMock.CriarToken(Arg.Any<Domain.Entity.User>())
+            .Returns(Task.FromResult(new Token("tok")));
+
+        _jobMock.AddJobAsync(Arg.Any<SyncData<Domain.Entity.User>>())
+            .Returns(ValueTask.CompletedTask);
+
+        var result = await UpdateUser().HandleAsync(command);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Name.Should().Be("newName");
+        result.Value.Email.Should().Be("new@email");
+        result.Value.Token.Should().Be("tok");
+        user.PasswordHash.Should().Be(initialHash);
+
+        await _dbMock.Received(1).SaveChangesAsync();
+        await _tokenMock.Received(1).CriarToken(Arg.Any<Domain.Entity.User>());
+        await _jobMock.ReceivedWithAnyArgs(1).AddJobAsync(default!);
+
+    }
+
+    [Fact]
+    public async Task HandleAsync_DeveAtualizar_ComSenha()
+    {
+        var userId = Guid.NewGuid();
+        var initialHash = BCrypt.Net.BCrypt.HashPassword("initial-hash");
+        var newPassword = BCrypt.Net.BCrypt.HashPassword("UPDATE-hash");
+
+        var user = new Domain.Entity
+            .User(userId, DateTime.UtcNow, DateTime.UtcNow, "oldName", initialHash, "old@email");
+
+        var command = new UpdateUserCommand(userId.ToString(), string.Empty, string.Empty, newPassword);
+
+        _validatorMock
+              .ValidateAsync(Arg.Any<UpdateUserCommand>())
+              .Returns(new ValidationResult());
+
+        _dbMock.Users.FindAsync(Arg.Any<Guid>())!
+            .Returns(ValueTask.FromResult(user));
+
+        _dbMock.SaveChangesAsync().Returns(Task.FromResult(1));
+        _tokenMock.CriarToken(Arg.Any<Domain.Entity.User>())
+            .Returns(Task.FromResult(new Token("tok")));
+
+        _jobMock.AddJobAsync(Arg.Any<SyncData<Domain.Entity.User>>())
+            .Returns(ValueTask.CompletedTask);
+
+        var result = await UpdateUser().HandleAsync(command);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Name.Should().Be("oldName");
+        result.Value.Email.Should().Be("old@email");
+        result.Value.Token.Should().Be("tok");
+
+        user.PasswordHash.Should().NotBe(initialHash);
+        BCrypt.Net.BCrypt.Verify(newPassword, user.PasswordHash).Should().BeTrue();
+
+        await _dbMock.Received(1).SaveChangesAsync();
+        await _tokenMock.Received(1).CriarToken(Arg.Any<Domain.Entity.User>());
+        await _jobMock.ReceivedWithAnyArgs(1).AddJobAsync(default!);
+
+
+
+    }
+
 
 
 }
