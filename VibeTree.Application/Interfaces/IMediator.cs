c87beb624ba700b3b4bdf5f48a62eb88651a18ad
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-06-06 21:53:25 -0300

    feat : add interface de mediação de instacias

---
+++ VibeTree.Application/Interfaces/IMediator.cs
@@ -0,0 +1,8 @@
+﻿using VibeTree.Application.Result;
+
+namespace VibeTree.Application.Interfaces;
+
+public interface IMediator
+{
+    public Task<Result<IResponse>> SendAsync<IResponse>(IRequest<IResponse> resquest);
+}
