bd1673e67a133e28f59c3e94f2d141b3dac78ac3
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-06-06 21:52:33 -0300

    feat : add interface para o banco de leitura e escrita

---
+++ VibeTree.Application/Interfaces/IAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Domain.Entity;
+
+
+namespace VibeTree.Application.Interfaces;
+
+public interface IAppDbContext
+{
+    public DbSet<Domain.Entity.User> Users { get; set; }
+    public DbSet<Link> Links { get; set; }
+    public DbSet<Domain.Entity.Perfil> perfils { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+}
