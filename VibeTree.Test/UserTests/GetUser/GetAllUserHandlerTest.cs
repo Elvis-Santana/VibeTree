a1cb54a42e0ce2febac6d59972028c98cfc54735
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-06-06 21:23:39 -0300

    fix : listar arquivo

--- VibeTree.Test/UserTests/GetUser/GetAllUserHandlerTest.cs
+++ VibeTree.Test/UserTests/GetUser/GetAllUserHandlerTest.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using VibeTree.Application.Interfaces;
 using VibeTree.Application.User;
 using VibeTree.Application.User.Get;
-using VibeTree.Infrastructure.AppDbContext;
 using VibeTree.User.CreateUser;
 
 namespace VibeTree.Test.UserTests.GetUser;
@@ -17,43 +16,6 @@ namespace VibeTree.Test.UserTests.GetUser;
 public class GetAllUserHandlerTest
 {
 
-    private readonly AppDbContext _context;
-
-    public GetAllUserHandlerTest()
-    {
-        var option =  new DbContextOptionsBuilder<AppDbContext>()
-          .UseInMemoryDatabase(Guid.NewGuid().ToString())
-          .Options;
-
-        _context = new (option);
-    }
-
-    [Fact]
-    public async Task GetAllUserHandler_Should_Return_UserResponse()
-    {
-        var faker = new Faker<Domain.Entity.User>("pt_BR")
-            .CustomInstantiator(f => new Domain.Entity.User(
-                Guid.NewGuid(),
-                DateTime.Now,
-                DateTime.Now,
-                f.Name.FullName(),
-                f.Internet.Email(),
-                f.Internet.Password()
-            ));
-
-        var users = faker.Generate(5);
-
-        _context.Users.AddRange(users);
-        await _context.SaveChangesAsync();
-
-        IHandler<GetAllUserQuery, List<UserResponse>> queryHandler = new GetAllUserHandler(_context);
-
-        List<UserResponse> result = await queryHandler
-            .HandleAsync(new GetAllUserQuery());
-
-        result.Should().NotBeNull();
-        result.Should().BeOfType<List<UserResponse>>();
-        result.Count.Should().Be(5);
-    }
+    
 
 }
