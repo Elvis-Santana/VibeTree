56aa0ef288dcec825a9ef8344184e38824d78099
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-06-21 22:57:06 -0300

    refactor :testes add CustomWebApplicationFactory

--- VibeTree.Test/UserTests/CreateTest/Integracao/CreateUserHandlerIntegracao.cs
+++ VibeTree.Test/UserTests/CreateTest/Integracao/CreateUserHandlerIntegracao.cs
@@ -1,84 +1,88 @@
 ﻿using Bogus;
 using FluentAssertions;
-using NSubstitute.Extensions;
-using System.Threading;
-using VibeTree.Application;
-using VibeTree.Application.Auth;
-using VibeTree.Application.Interfaces;
-using VibeTree.Application.Interfaces.IQueryJob;
-using VibeTree.Application.QuerySync;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System.Net.Http.Json;
 using VibeTree.Application.Result;
 using VibeTree.Application.User;
-using VibeTree.Domain;
-using VibeTree.Infrastructure.Query;
+using VibeTree.Infrastructure.AppDbContext;
 using VibeTree.User.CreateUser;
 namespace VibeTree.Test.UserTests.CreateTest.Integracao;
 
-public class CreateUserHandlerIntegracao 
+public class CreateUserHandlerIntegracao(CustomWebApplicationFactory factory) : IClassFixture<CustomWebApplicationFactory>
 {
-
-  
+    private readonly HttpClient client = factory.CreateClient();
+    private readonly IServiceProvider services = factory.Services;
 
     [Fact]
-    public async Task CreateUserHandler_Should_Create_User()
+    public async Task Deve_Criar_Usuario_Com_Sucesso()
     {
+        // Arrange
 
-        await using var db = new DbContextBuildConfig();
-        var context = await db.CriarContextoWritePreparadoAsync();
+        using (var scope = services.CreateScope())
+        {
+            var writeDb = scope.ServiceProvider.GetRequiredService<WriteDbContext>();
+            await writeDb.Database.EnsureDeletedAsync();
+            await writeDb.Database.EnsureCreatedAsync();
 
-
-        var config = JwtBuildConfig.BuildConfig();
-        var service = new TokenService(config);
-
-        CreateUserCommand user = new Faker<CreateUserCommand>("pt_BR")
-        .CustomInstantiator(f =>
-        new(f.Person.FullName, f.Internet.Password(), f.Person.Email)).Generate();
-
-        IQueryJobSynchronize<SyncData<Domain.Entity.User>> queryJobSynchronize = new QueryJobSynchronizeUserDb();
-        IMediator mediator = FactoryMed
-            .CreateMediatorWithHandler(new CreateUserHandler(context, service, new CreateUserValidator(), queryJobSynchronize));
+            var readDb = scope.ServiceProvider.GetRequiredService<ReadDbContext>();
+            await readDb.Database.EnsureDeletedAsync();
+            await readDb.Database.EnsureCreatedAsync();
+        }
 
 
-        Result<Userlogin> result = await mediator.SendAsync(user);
 
-        result.Should().NotBeNull();
-        result.IsSuccess.Should().BeTrue();
-        result.Value.Name.Should().Be(user.Name);
-        result.Value.Email.Should().Be(user.Email);
-        result.Value.Id.Should().NotBeEmpty();
-        result.Value.Token.Should().NotBeNullOrWhiteSpace();
+        CreateUserCommand command = new Faker<CreateUserCommand>("pt_BR")
+            .CustomInstantiator(f =>
+                new CreateUserCommand(
+                    f.Person.FullName,
+                    f.Internet.Password(),
+                    f.Person.Email))
+            .Generate();
 
+        // Act
 
-        queryJobSynchronize.HasPending.Should().BeTrue();
-       await foreach (var job in queryJobSynchronize.ReadAllAsync())
-       {
-            job.Operation.Should().Be(SyncOperation.Create);
-            job.Item.Id.Should().Be(result.Value.Id);
-            BCrypt.Net.BCrypt.Verify(user.Password, job.Item.PasswordHash).Should().BeTrue();
-
-            break;
-       }
+        var response =
+            await client.PostAsJsonAsync<CreateUserCommand>(
+                "/user",
+                command);
 
 
+        //assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        using (var scope = services.CreateScope())
+        {
+            var readDb = scope.ServiceProvider.GetRequiredService<ReadDbContext>();
+            var usuarioSalvo = await readDb.Users.FirstOrDefaultAsync(u => u.Email == command.Email);
+            usuarioSalvo.Should().NotBeNull();
+        }
     }
 
+
+
     [Fact]
-    public async Task CreateUserHandler_Should_Return_Error_When_Invalid_Data()
+    public async Task Deve_Retornar_BadRequest_Por_Informacores_Invalidas()
     {
-        await using var db = new DbContextBuildConfig();
-        IWriteDbContext context = await db.CriarContextoWritePreparadoAsync();
+        using (var scope = services.CreateScope())
+        {
+            var writeDb = scope.ServiceProvider.GetRequiredService<WriteDbContext>();
+            await writeDb.Database.EnsureDeletedAsync();
+            await writeDb.Database.EnsureCreatedAsync();
 
-        var config = JwtBuildConfig.BuildConfig();
-        var service = new TokenService(config);
+            var readDb = scope.ServiceProvider.GetRequiredService<ReadDbContext>();
+            await readDb.Database.EnsureDeletedAsync();
+            await readDb.Database.EnsureCreatedAsync();
+        }
 
-        CreateUserCommand user = new(string.Empty, string.Empty, string.Empty);
-        IQueryJobSynchronize<SyncData<Domain.Entity.User>> queryJobSynchronize = new QueryJobSynchronizeUserDb();
+        CreateUserCommand command = new(string.Empty, string.Empty, string.Empty);
+  
+        var response = await client.PostAsJsonAsync<CreateUserCommand>("/user",command);
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        response.IsSuccessStatusCode.Should().BeFalse();
 
-        IMediator mediator = FactoryMed.CreateMediatorWithHandler(new CreateUserHandler(context, service, new CreateUserValidator(), queryJobSynchronize));
-
-
-        Result<Userlogin> result = await mediator.SendAsync(user);
-        result.Should().NotBeNull();
+        var result = await response.Content.ReadFromJsonAsync<Result<Userlogin>>();
 
         result.IsSuccess.Should().BeFalse();
         result.Errors.Contains(Error.EmailAddress).Should().BeTrue();
@@ -89,11 +93,6 @@ public class CreateUserHandlerIntegracao
 
         result.Errors.Contains(Error.PasswordEmpty).Should().BeTrue();
         result.Errors.Contains(Error.PasswordMinimumLength).Should().BeTrue();
-
-        queryJobSynchronize.HasPending.Should().BeFalse();
-
-
-
 
     }
 }
