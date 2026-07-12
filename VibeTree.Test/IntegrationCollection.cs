80f4f9388d99ee6e7d0f334f2ece2ea3ebcf1ee4
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-07-12 17:01:46 -0300

    feat : add class  que implementa ICollectionFixture

---
+++ VibeTree.Test/IntegrationCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibeTree.Test;
+
+[CollectionDefinition(Name)]
+public class IntegrationCollection : ICollectionFixture<CustomWebApplicationFactory>
+{
+    public const string Name = "Integration";
+}
