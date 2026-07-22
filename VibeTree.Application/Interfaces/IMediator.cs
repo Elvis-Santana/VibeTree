0a6247083ac7728bfba4b2950a4e032cb4cf30b5
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-07-22 18:21:32 -0300

    refactor: delete arquivos

--- VibeTree.Application/Interfaces/IMediator.cs
+++
@@ -1,8 +0,0 @@
-﻿using VibeTree.Application.Common;
-
-namespace VibeTree.Application.Interfaces;
-
-public interface IMediator
-{
-    public Task<Result<IResponse>> SendAsync<IResponse>(IRequest<IResponse> resquest);
-}
