3eb0dabc9c5acbe1e72fc90511ee7eccf4113bac
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-07-22 11:01:23 -0300

    feat: add SyncUserPerfilCreatedEvent para evento de sincronizar banco de dados

---
+++ VibeTree.Application/User/Create/Events/SyncUserPerfilCreatedEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeTree.Domain.Entity;
+
+namespace VibeTree.Application.User.Create.Events;
+
+public  record SyncUserPerfilCreatedEvent(Domain.Entity.User User , Domain.Entity.Perfil Perfil);
+
+
