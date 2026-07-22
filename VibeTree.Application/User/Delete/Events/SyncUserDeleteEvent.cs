c67d4c670c1614714d97f1669b2f40ad6121b7cb
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-07-22 18:30:28 -0300

    feat : add SyncUserDeleteEvent e SyncUserDeleteHandler

---
+++ VibeTree.Application/User/Delete/Events/SyncUserDeleteEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibeTree.Application.User.Delete.Events;
+
+public record SyncUserDeleteEvent(Domain.Entity.User User);
+
+
