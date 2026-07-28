bcbcf3111ecc55a0e88d1645d42f1a1a8fed55b3
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-07-28 17:05:01 -0300

    feat :add ToJson para transformar object em Json

---
+++ VibeTree/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace VibeTree.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToJson(this object obj)=> JsonSerializer.Serialize(obj, new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        });
+    
+}
