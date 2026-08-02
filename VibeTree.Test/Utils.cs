c62175c5aaa91c0a3866ffd1982c8f24e489302a
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-08-02 17:27:29 -0300

    feat :add  função para retornar entity de user e perfil

--- VibeTree.Test/Utils.cs
+++ VibeTree.Test/Utils.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Bogus;
+using Microsoft.Extensions.Hosting;
+using VibeTree.Features.User.GetUser.GetAllUser;
+using VibeTree.Shared.Entity;
 using Wolverine;
 using Wolverine.Tracking;
 
@@ -15,5 +18,32 @@ public static class Utils
         await host.TrackActivity().Timeout(TimeSpan.FromSeconds(10)).ExecuteAndWaitAsync(action);
 
         return response;
+    }
+
+    public static (User user, Perfil perfil) GetUserAndPerfil()
+    {
+        User user = new Faker<User>("pt_BR")
+         .CustomInstantiator(f => new(Guid.NewGuid(),
+             DateTime.Now,
+             DateTime.Now,
+             f.Internet.UserName(),
+             f.Internet.Password(),
+             f.Internet.Email())
+         ).Generate();
+
+        Perfil perfil = new Faker<Perfil>("pt_BR")
+           .CustomInstantiator(f =>
+               new(Guid.NewGuid(),
+               DateTime.Now,
+               DateTime.Now,
+               f.Internet.Color(),
+               f.Lorem.Text(),
+               f.Image.LoremFlickrUrl(),
+               f.Internet.UserName(),
+               user.Id)
+          ).Generate();
+
+        return (user, perfil);
+
     }
 }
