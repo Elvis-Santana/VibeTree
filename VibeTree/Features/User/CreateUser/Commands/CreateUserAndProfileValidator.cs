b899507f7a435da814206f73a8b1faf472970019
Elvis Santana <elvissantanaguimaraes@gmail.com>
2026-08-03 20:52:00 -0300

    refactor : mudar ordem

--- VibeTree/Features/User/CreateUser/Commands/CreateUserAndProfileValidator.cs
+++ VibeTree/Features/User/CreateUser/Commands/CreateUserAndProfileValidator.cs
@@ -12,14 +12,15 @@ public class CreateUserAndProfileValidator : AbstractValidator<CreateUserAndProf
             .NotEmpty().WithMessage(Error.NameEmpty.Message)
             .MinimumLength(3).WithMessage(Error.NameMinimumLength.Message);
 
-        RuleFor(x => x.Email)
-           .NotEmpty().WithMessage(Error.EmailEmpty.Message)
-           .EmailAddress(FluentValidation.Validators.EmailValidationMode.Net4xRegex)
-           .WithMessage(Error.EmailAddress.Message);
 
         RuleFor(x => x.Password)
             .NotEmpty().WithMessage(Error.PasswordEmpty.Message)
             .MinimumLength(6).WithMessage(Error.PasswordMinimumLength.Message);
+
+        RuleFor(x => x.Email)
+           .NotEmpty().WithMessage(Error.EmailEmpty.Message)
+           .EmailAddress(FluentValidation.Validators.EmailValidationMode.Net4xRegex)
+           .WithMessage(Error.EmailAddress.Message);
 
         RuleFor(p => p.Slug)
         .NotEmpty()
